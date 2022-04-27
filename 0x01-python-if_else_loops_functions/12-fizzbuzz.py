66f288c437323b0a22ce7d4774daa596f6f00631
ChrissLind <chrislindino@gmail.com>
2022-04-26 23:30:26 -0400

    task 12 fix?

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def fizzbuzz():
-    """Print the numbers from 1 to 100 separated by a space."""
 
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
