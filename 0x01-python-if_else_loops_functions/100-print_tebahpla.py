963b76f3dd2942fd70e24301f61bb5d0d5f65e7d
ChrissLind <chrislindino@gmail.com>
2022-04-26 23:33:54 -0400

    adv task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".cormat(chr(c - i)), end="")
+    print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
