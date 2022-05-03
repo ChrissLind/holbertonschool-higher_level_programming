897fdca0a9b1765e83b8e18f27b26f9ec01d81a2
ChrissLind <chrislindino@gmail.com>
2022-05-02 22:07:50 -0400

    task3 typo

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def print_reverse_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=[]):
     if ininstance(my_list, list):
         my_list.reverse()
         for i in my_list:
