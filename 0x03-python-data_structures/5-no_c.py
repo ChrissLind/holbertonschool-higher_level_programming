ab64d563ce0289a5827ef0c93272b5593e32d62d
ChrissLind <chrislindino@gmail.com>
2022-05-02 22:15:00 -0400

    task 5 space after bracket

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    copy = [ x for x in my_string if x != 'c' and x != 'C']
+
+    copy = [x for x in my_string if x != 'c' and x != 'C']
     return ("".join(copy))
