9688ff673c3e9585c54d12b932cca726be1be95b
ChrissLind <chrislindino@gmail.com>
2022-05-05 16:31:54 -0400

    task 1, weird typo

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
 
     new_list = my_list[:]
     for i in range(len(new_list)):
-        if nre_list[i] == search:
-            nre_list[i] = replace
+        if new_list[i] == search:
+            new_list[i] = replace
     return (new_list)
