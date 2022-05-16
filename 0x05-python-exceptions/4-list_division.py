9720ded4f2b413717adc3e3b8adb14cd44ccb253
ChrissLind <chrislindino@gmail.com>
2022-05-16 19:49:52 -0400

    task 4 typo

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
             div = 0
         except ZeroDivisionError:
-            print("divsion by 0")
+            print("division by 0")
             div = 0
         except IndexError:
             print("out of range")
