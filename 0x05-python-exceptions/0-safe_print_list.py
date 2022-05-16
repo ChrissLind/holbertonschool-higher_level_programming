7f9013b9294277d256e6ced7bcc76e7312a62256
ChrissLind <chrislindino@gmail.com>
2022-05-16 14:35:58 -0400

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from tempfile import TemporaryFile
+
+
+def safe_print_list(my_list=[], x=0):
+    ret = 0
+
+    for i in range(x):
+        try:
+            print("{}":.format(my_list[i]), end="")
+            ret += 1
+            except IndexError:
+                break
+            print("")
+            return (ret)
