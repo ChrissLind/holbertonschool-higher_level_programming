7d5e38a5788e9bf61fc2236c033df57d732321a7
ChrissLind <chrislindino@gmail.com>
2022-05-16 14:57:18 -0400

    pycode commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     ret = 0
 
     for i in range(x):
         try:
-            print("{}":.format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             ret += 1
-            except IndexError:
-                break
-            print("")
-            return (ret)
+        except IndexError:
+            break
+        print("")
+        return (ret)
