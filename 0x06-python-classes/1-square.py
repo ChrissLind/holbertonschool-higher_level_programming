0c8e5bb4c9b2d12b312d823f423d72222eddaca7
ChrissLind <chrislindino@gmail.com>
2022-05-17 16:39:23 -0400

    adding comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" Module contain class square """
 
 
 class Square:
+    """ Define Square """
     def __init__(self, size):
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,6 +2,7 @@
 
 
 class Square:
+    
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
