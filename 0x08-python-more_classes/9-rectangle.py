aba034177ab257d4d60538030ddf87ce25e6ddc3
ChrissLind <chrislindino@gmail.com>
2022-05-25 16:30:21 -0400

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -84,7 +84,7 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
-    
+
     @classmethod
     def square(cls, size=0):
         """Return a new Rectangle with width and height equal to size.
