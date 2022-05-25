d764da6ce6323715b50099cf72628302944c8cf4
ChrissLind <chrislindino@gmail.com>
2022-05-25 14:53:44 -0400

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represent a rectangle."""
 
-    counter = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -15,7 +15,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.counter += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -75,4 +75,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        Rectangle.counter -= 1
+        Rectangle.number_of_instances -= 1
