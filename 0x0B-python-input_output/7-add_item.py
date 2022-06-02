e5ba0e665496421ec0081f64cf89e26ee884cd81
ChrissLind <chrislindino@gmail.com>
2022-06-02 00:01:15 -0400

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,6 @@
 """Add all arguments to a Python list and save to a file."""
 
 import sys
-import os
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
