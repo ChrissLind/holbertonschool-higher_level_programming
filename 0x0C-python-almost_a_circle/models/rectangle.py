81dc6345decc836d85cccf334c722968613358ee
ChrissLind <chrislindino@gmail.com>
2022-06-08 16:09:39 -0400

    task 8 update#0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,23 @@ class Rectangle(Base):
         h = self.height
         i = self.id
         return f"[Rectangle] ({i}) {x}/{y} - {w}/{h}"
+
+    def update(self, *args):
+        """Update the Rectangle."""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
