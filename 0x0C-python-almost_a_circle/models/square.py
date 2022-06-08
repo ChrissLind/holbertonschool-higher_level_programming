10ddf2d5defa2f11f10cb186cf6f649b19e2bf1b
ChrissLind <chrislindino@gmail.com>
2022-06-08 16:19:26 -0400

    adding square and beginning tasks

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square."""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
