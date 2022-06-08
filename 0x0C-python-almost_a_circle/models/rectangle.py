dc466259729ff720f9d1bb130e95ea430494f2c3
ChrissLind <chrislindino@gmail.com>
2022-06-08 11:38:48 -0400

    Rectangle update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,7 @@
 from models.base import Base
 
 
-class Rectangle:
+class Rectangle(Base):
     """Represents a rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -13,24 +13,56 @@ class Rectangle:
         self.height = height
         self.x = x
         self.y = y
-        super().__init__('id')
+        super().__init__(id)
 
     @property
     def width(self):
         """Get/set the width of the Rectangle."""
         return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("Width must be an integer")
+        if value <= 0:
+            raise ValueError("Width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
         """Get/set the height of the Rectangle"""
         return self.__height
 
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("Height must be an integer")
+        if value <= 0:
+            raise ValueError("Height must be > 0")
+        self.__height = value
+
     @property
     def x(self):
         """Get/set the x coordinate of the Rectangle"""
         return self.__x
 
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
     @property
     def y(self):
         """Get/set the y coordinate of the Rectangle"""
         return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
