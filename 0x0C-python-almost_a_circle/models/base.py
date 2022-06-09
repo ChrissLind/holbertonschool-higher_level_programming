a88932fb5676bcf1a133cd5ce72cd57353f5d238
ChrissLind <chrislindino@gmail.com>
2022-06-09 10:45:30 -0400

    Dummy fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,6 +44,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """Dummy"""
-        Dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            Dummy = cls(1, 1)
+        else:
+            Dummy = cls(1)
         Dummy.update(**dictionary)
         return Dummy
