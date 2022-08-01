aa72c5654365df5b6ecfa46f950935cc3127450f
ChrissLind <chrislindino@gmail.com>
2022-08-01 06:22:48 -0400

    tasks 0-6 & README

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let i = 0;
+const myVar =
+['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (i; myVar[i]; i++) {
+  console.log(myVar[i]);
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+for (const loop of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(loop);
+}
