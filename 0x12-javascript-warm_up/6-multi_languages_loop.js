a82b37483a4f204dd7086a6747ccaa37c22e8204
ChrissLind <chrislindino@gmail.com>
2022-08-01 12:15:53 -0400

    captial S

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const loop of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const loop of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(loop);
 }
