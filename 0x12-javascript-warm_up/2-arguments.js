1ab6a1c94b4b77e81edfd393ed80fbb14a6fa854
ChrissLind <chrislindino@gmail.com>
2022-08-01 09:42:45 -0400

    typo delete

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');1
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
