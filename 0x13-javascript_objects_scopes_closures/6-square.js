d08805ee038f695889c3897b5ed27c8d32b5d69d
ChrissLind <chrislindino@gmail.com>
2022-08-02 13:31:15 -0400

    test again

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
-    if (typeof c === undefined) {
+    if (c === undefined) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
