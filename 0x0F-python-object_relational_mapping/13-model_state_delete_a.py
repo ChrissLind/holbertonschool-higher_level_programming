58538aab8e79b02ccfba3f817a9a0b6aec9c9264
ChrissLind <chrislindino@gmail.com>
2022-08-07 21:53:45 -0400

    edit comment

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Deletes all State objects with a name containing
-    the letter a from the database hbtn_0e_6_usa."""
+the letter a from the database hbtn_0e_6_usa."""
 
 import sys
 from sqlalchemy import create_engine
