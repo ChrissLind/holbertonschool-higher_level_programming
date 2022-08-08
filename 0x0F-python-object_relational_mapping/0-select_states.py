373f178c70c1cdc733f5631e5e397d9efaaf975a
ChrissLind <chrislindino@gmail.com>
2022-08-07 20:56:31 -0400

    checker test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""Lists all states from the database hbtn_0e_0_usa."""
+
 import sys
 import MySQLdb
 
