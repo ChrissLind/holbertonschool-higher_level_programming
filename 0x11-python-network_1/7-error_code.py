d4935393ddc6c50d3aa1a1486b9a8aa2490062d4
ChrissLind <chrislindino@gmail.com>
2022-08-19 17:54:09 -0400

    task 7?

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
