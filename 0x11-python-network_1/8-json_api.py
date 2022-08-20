fdf330edfbc4b69bbe5a4dda3f4aa645417ac3c0
ChrissLind <chrislindino@gmail.com>
2022-08-20 00:00:06 -0400

    line to long

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter."""
+"""Sends a POST request to URL with a given letter."""
 import sys
 import requests
 
